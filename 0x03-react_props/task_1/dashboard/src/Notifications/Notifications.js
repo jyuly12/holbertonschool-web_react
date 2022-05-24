5b0b190369578031b59ce4e80b895393bc0abdb9
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-05-24 17:55:27 -0500

    Add files

---
+++ 0x03-react_props/task_1/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Notifications.css';
+import {getLatestNotification} from '../utils/utils';
+
+function Notification() {
+  return (
+    <div className='Notifications'>
+      <button style={{"ariaLabel": "Close"}} 
+        onClick={console.log("Close button has been clicked")}>
+        x
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+          <li data="default">New course available</li>
+          <li data="urgent">New resume available</li>
+          <li data="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+        </ul>
+    </div>
+  );
+};
+
+export default Notification;
