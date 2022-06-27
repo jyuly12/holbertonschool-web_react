2f460b6199e9f38ca1dad2c487bdac39e344c332
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-06-27 18:28:43 -0500

    Add files

--- 0x06-react_state/task_1/dashboard/src/Login/Login.js
+++ 0x06-react_state/task_1/dashboard/src/Login/Login.js
@@ -1,30 +1,71 @@
 import React from 'react';
 import {StyleSheet, css} from 'aphrodite';
 
-function Login(){
-  return(
-    <React.Fragment>
-      <div className={css(styles.AppLogin)}>
-        <p className={css(styles.p)}>
-          Login to access the full dashboard
-        </p>
-        <div className={css(styles.Login)}>
-          
-          <div >
-            <label htmlFor='email' className={css(styles.label)}>Email:</label>
-            <input type='email' id='email' className={css(styles.input)}/>
-          </div>
+class Login extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      email: '',
+      password: '',
+      enableSubmit: false,
+      };
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+  }
 
-          <div >
-            <label htmlFor='password' className={css(styles.label)}>Password:</label>
-            <input type='password' id='password' className={css(styles.input)}/>
-          </div>
+  handleLoginSubmit(event) {
+    event.preventDefault();
+    this.setState({ isLoggedIn: true });
+  }
 
-          <button className={css(styles.button)}>OK</button>
+  handleChangeEmail(event) {
+    const { value } = event.target;
+    const { password } = this.state;
+
+    if (value !== "" && password !== "") this.setState({ enableSubmit: true });
+    else this.setState({ enableSubmit: false });
+
+    this.setState({ email: event.target.value });
+  }
+
+  handleChangePassword(event) {
+    const { value } = event.target;
+    const { email } = this.state;
+
+    if (email !== "" && value !== "") this.setState({ enableSubmit: true });
+    else this.setState({ enableSubmit: false });
+
+    this.setState({ password: event.target.value });
+  }
+
+  render (){
+    const {isLoggedIn, email, password, enableSubmit} = this.state
+    return(
+      <React.Fragment>
+        <div className={css(styles.AppLogin)}>
+          <p className={css(styles.p)}>
+            Login to access the full dashboard
+          </p>
+          <div >
+            <form action="" onSubmit={this.handleLoginSubmit} className={css(styles.Login)}>
+              <div >
+                <label htmlFor='email' className={css(styles.label)} >Email:</label>
+                <input type='email' id='email' className={css(styles.input)} value={email} onChange={this.handleChangeEmail}/>
+              </div>
+
+              <div >
+                <label htmlFor='password' className={css(styles.label)}>Password:</label>
+                <input type='password' id='password' className={css(styles.input)} value={password} onChange={this.handleChangePassword}/>
+              </div>
+              <input type='submit' className={css(styles.button)} disabled={!enableSubmit} />
+            </form>
+          </div>
         </div>
-      </div>
-    </React.Fragment>
-  )
+      </React.Fragment>
+    )
+  }
 }
 const screenSize = {
   small: '@media screen and (max-width: 900px)',
@@ -61,7 +102,7 @@ const styles = StyleSheet.create({
     marginLeft: '1rem',
     borderRadius: '5px',
     borderColor: '#E5E4E4',
-    maxWidth: '2.5rem',
+    maxWidth: '3.5rem',
     [screenSize.small]: {
       marginTop: '1rem',
       marginLeft: '0',
